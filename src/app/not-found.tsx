d7429cb67c5eb8d9453edbb4913ef2740cb9dc16
Vũ Văn Hoàng <hoangvuvan677@gmail.com>
2025-02-09 20:51:15 +0700

    Cau hinh not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,18 @@
+export default function NotFound() {
+    return (
+        <main className="grid min-h-full place-content-center">
+            Hoang dep trai nhat the gioi
+
+            <a href="#">
+  <svg></svg>
+  <span className="sr-only">Settings</span>
+  sdffdf
+</a>
+
+<a href="#">
+  <svg></svg>
+  <span className="sr-only sm:not-sr-only">Settings</span>
+</a>
+        </main>
+    );
+}
