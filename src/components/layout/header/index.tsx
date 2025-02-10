358636857dcfefdafaef154d7430a7c7b2ed8119
Vũ Văn Hoàng <hoangvuvan677@gmail.com>
2025-02-10 23:22:20 +0700

    update header: button danh nhap/ky

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,10 +1,16 @@
+'use client';
 import { Search, ShoppingCart, User } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Logo } from "@/components/logo";
 import { Navbar } from "../navbar";
+import { useState } from "react";
 
 export function Header() {
+  const [ isLogin, setIsLogin ] = useState(false);
+  const [ dropdownOpen, setDropdownOpen ] = useState(false);
+
+  const toggleDropdown = () => setDropdownOpen(true);
   return (
     <header className="border-b">
       {/* Include Logo, search place, login, logout, cart */}
@@ -28,17 +34,28 @@ export function Header() {
         </div>
 
         {/* control place */}
-        <div className="flex items-end space-x-4">
-          <Button variant="ghost" size="icon" className="w-10 h-10">
-            <User className="!h-10 !w-10 p-2 " role="button" color="gray"/>
-          </Button>
-          <div role="button" className="flex space-x-2 items-center hover:bg-gray-100 p-2 rounded-lg">
-            <User className="" role="button" color="gray"/>
-            <span className="text-gray-500">Tài khoản</span>
-          </div>
+        <div className="flex items-end space-x-8">
           <Button variant="ghost" size="icon" className="w-10 h-10">
             <ShoppingCart className="!h-10 !w-10 p-2" role="button" color="gray"/>
           </Button>
+          <div className="relative">
+            {isLogin ? (
+              <div 
+                role="button" 
+                className="flex space-x-2 items-center hover:bg-gray-100 p-2 rounded-lg"
+                onClick={toggleDropdown}
+              >
+                <User className="" role="button" color="gray"/>
+                <span className="text-gray-500">Tài khoản</span>
+              </div>
+            ) : (
+              <div className="flex space-x-0.5 justify-between items-center cursor-pointer">
+                <a href="/login" className="block px-1 py-2 text-gray-800 hover:text-green-400">Đăng nhập</a>
+                <p>|</p>
+                <a href="/register" className="block px-1 py-2 text-gray-800 hover:text-green-400">Đăng ký</a>
+              </div>
+            )}
+          </div>
         </div>
       </div>
 
