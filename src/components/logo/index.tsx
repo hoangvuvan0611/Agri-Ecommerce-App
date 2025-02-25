84e67b969d510f2175b823111cd9e344ba855555
Vũ Văn Hoàng <hoangvuvan677@gmail.com>
2025-02-25 23:33:19 +0700

    Fix anh logo

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -6,8 +6,12 @@ export function Logo() {
     return (
         <Link href={REF_HOME}>
             <Image 
-                src={'https://fruitio.monamedia.net/wp-content/uploads/2024/04/Mona-Media-e1712831506867.png'} alt={"Logo"}
-                width={150} height={200}
+                src={'/logo/logo.png'}
+                alt={"Logo"}
+                width={150}
+                height={200}
+                priority={true}
+                quality={100}
             />
         </Link>
     );
