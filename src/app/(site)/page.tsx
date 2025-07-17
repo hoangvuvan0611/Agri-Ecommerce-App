bea804c0717f6bb8995e950e0a4661bf5fd1916b
Vũ Văn Hoàng <hoangvuvan677@gmail.com>
2025-07-17 23:20:23 +0700

    update

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -7,6 +7,8 @@ import ProductsIntro from '../page/products';
 import RecommendedProducts from '@/components/product/RecommendedProducts';
 
 export default function FruitStorePage () {
+  console.log('API_URL:', process.env.NEXT_PUBLIC_API_BASE_URL);
+  console.log('API_URL:', process.env.NEXT_PUBLIC_API_MINIO_URL);
   return (
     <div className="mx-2 sm:mx-4 md:mx-10 lg:mx-20">
       {/* Hero Section */}
