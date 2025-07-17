26814dfcc655810231f6ca14d77f43476ea35b45
Vũ Văn Hoàng <hoangvuvan677@gmail.com>
2025-07-17 08:03:18 +0700

    update: bo sung

--- src/app/page/products/index.tsx
+++ src/app/page/products/index.tsx
@@ -83,107 +83,116 @@ export default function ProductsIntro ({
         <div className="mt-10 md:mt-20">
             <h2 className="text-lg md:text-2xl font-bold text-gray-600 px-2 md:px-0">{title}</h2>
             <div className="grid grid-cols-1 md:grid-cols-12 gap-4 min-h-[400px] md:min-h-[770px] py-4 md:py-[30px]">
-                <div className="col-span-1 md:col-span-3 relative bg-cover bg-center rounded-xl p-4 md:p-10 mb-4 md:mb-0 flex flex-col items-center justify-center">
-                    {/* Background image */}
-                    <div className="absolute inset-0 -z-10 rounded-md overflow-hidden"> 
-                        <Image
-                            src={image}
-                            alt="Organic Farm"  
-                            height={400}
-                            width={360}
-                            style={{objectFit: 'cover', objectPosition: 'center'}}
-                            priority={false}
-                            quality={100}
-                            className="rounded-md w-full h-full"
-                        />
-                    </div>
-                    <div className="text-base md:text-lg font-semibold text-center" style={styleTitleBg}>
-                        {titleBgBanner}
-                    </div>
-                    {hasButton && <Button className="bg-white hover:bg-lime-600 hover:text-white text-black mt-4 w-full md:w-auto">Mua ngay</Button>}
+                <div className="col-span-1 md:col-span-3 relative bg-cover bg-center rounded-xl p-4 md:p-10 mb-4 md:mb-0 flex flex-col items-center justify-center min-h-[200px] md:min-h-[400px]">
+                {/* Background image */}
+                <div className="absolute inset-0 -z-10 rounded-md overflow-hidden">
+                    <Image
+                    src={image}
+                    alt="Organic Farm"
+                    fill
+                    style={{ objectFit: 'cover', objectPosition: 'center' }}
+                    priority={false}
+                    quality={100}
+                    className="rounded-md w-full h-full"
+                    />
+                </div>
+
+                {/* Nội dung */}
+                <div
+                    className="text-sm sm:text-base md:text-lg font-semibold text-center"
+                    style={styleTitleBg}
+                >
+                    {titleBgBanner}
+                </div>
+
+                {hasButton && (
+                    <Button className="bg-white hover:bg-lime-600 hover:text-white text-black mt-4 w-full md:w-auto text-sm md:text-base">
+                    Mua ngay
+                    </Button>
+                )}
                 </div>
                 <div className="col-span-1 md:col-span-9">
-                <div className="grid grid-cols-2 md:grid-cols-5 gap-4 sm:gap-4 md:gap-6">
-                    {productList?.map((product: ProductType) => (
-                    <div
-                        key={product.id}
-                        className={`
-                        flex flex-col cursor-pointer transition-shadow
-                        rounded-xl
-                        hover:shadow-lg
-                        bg-white
-                        h-full
-                        border p-3
-                        md:border-0 md:p-0
-                        `}
-                        onClick={() => handleProductClick(product.id)}
-                    >
-                        <div className="p-0 md:p-4 h-full flex flex-col">
-                        {/* Ảnh sản phẩm */}
-                        <Link
-                            href={`/san-pham/${product?.slug}`}
-                            className="group relative overflow-hidden rounded-md h-32 sm:h-36 md:h-48"
+                    <div className="grid grid-cols-2 md:grid-cols-5 gap-4 sm:gap-4 md:gap-6">
+                        {productList?.map((product: ProductType) => (
+                        <div
+                            key={product.id}
+                            className={`
+                            flex flex-col cursor-pointer transition-shadow
+                            rounded-xl
+                            hover:shadow-lg
+                            bg-white
+                            h-full
+                            border p-3
+                            md:border-0 md:p-0
+                            `}
+                            onClick={() => handleProductClick(product.id)}
                         >
-                            <div className="relative w-full h-full">
-                            <Image
-                                src={`${process.env.NEXT_PUBLIC_API_MINIO_URL}${product?.path}`}
-                                alt={product.name}
-                                fill
-                                style={{ objectFit: 'cover', objectPosition: 'center' }}
-                                quality={100}
-                                className="transition-transform duration-1000 ease-in-out group-hover:scale-110"
-                            />
+                            <div className="p-0 md:p-4 h-full flex flex-col">
+                            {/* Ảnh sản phẩm */}
+                            <Link
+                                href={`/san-pham/${product?.slug}`}
+                                className="group relative overflow-hidden rounded-md h-32 sm:h-36 md:h-48"
+                            >
+                                <div className="relative w-full h-full">
+                                <Image
+                                    src={`${process.env.NEXT_PUBLIC_API_MINIO_URL}${product?.path}`}
+                                    alt={product.name}
+                                    fill
+                                    style={{ objectFit: 'cover', objectPosition: 'center' }}
+                                    quality={100}
+                                    className="transition-transform duration-1000 ease-in-out group-hover:scale-110"
+                                />
+                                </div>
+
+                                {/* Nút yêu thích */}
+                                <Button
+                                title="Yêu thích"
+                                size="icon"
+                                variant="ghost"
+                                className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity"
+                                >
+                                <Heart className="w-5 h-5" />
+                                </Button>
+
+                                {/* Nút xem chi tiết */}
+                                <div
+                                title="Xem chi tiết"
+                                className="absolute bottom-1 right-1 bg-white p-2 rounded-md hover:bg-lime-600 hover:text-white opacity-0 group-hover:opacity-100 transition-opacity"
+                                >
+                                <Eye className="w-4 h-4" />
+                                </div>
+                            </Link>
+
+                            {/* Thông tin sản phẩm */}
+                            <div className="flex flex-col flex-1 justify-between mt-1 md:mt-2">
+                                <h3
+                                className="font-medium text-xs sm:text-sm line-clamp-2 h-8 sm:h-10 overflow-hidden"
+                                title={product.name}
+                                >
+                                {product.name}
+                                </h3>
+                                <div className="flex items-center justify-between mt-1">
+                                <span className="text-xs sm:text-sm font-semibold">
+                                    {product?.originalPrice?.toLocaleString()}đ
+                                </span>
+                                <Button
+                                    size="sm"
+                                    className="bg-lime-600 hover:bg-lime-700 px-2 md:px-4 text-xs sm:text-sm"
+                                    onClick={(e) => {
+                                    e.preventDefault();
+                                    handleAddToCart(product);
+                                    handleAddProductToCartClick(product.id);
+                                    }}
+                                >
+                                    <ShoppingCart className="w-4 h-4 mr-1" />
+                                    Thêm
+                                </Button>
+                                </div>
                             </div>
-
-                            {/* Nút yêu thích */}
-                            <Button
-                            title="Yêu thích"
-                            size="icon"
-                            variant="ghost"
-                            className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity"
-                            >
-                            <Heart className="w-5 h-5" />
-                            </Button>
-
-                            {/* Nút xem chi tiết */}
-                            <div
-                            title="Xem chi tiết"
-                            className="absolute bottom-1 right-1 bg-white p-2 rounded-md hover:bg-lime-600 hover:text-white opacity-0 group-hover:opacity-100 transition-opacity"
-                            >
-                            <Eye className="w-4 h-4" />
-                            </div>
-                        </Link>
-
-                        {/* Thông tin sản phẩm */}
-                        <div className="flex flex-col flex-1 justify-between mt-1 md:mt-2">
-                            <h3
-                            className="font-medium text-xs sm:text-sm line-clamp-2 h-8 sm:h-10 overflow-hidden"
-                            title={product.name}
-                            >
-                            {product.name}
-                            </h3>
-                            <div className="flex items-center justify-between mt-1">
-                            <span className="text-xs sm:text-sm font-semibold">
-                                {product?.originalPrice?.toLocaleString()}đ
-                            </span>
-                            <Button
-                                size="sm"
-                                className="bg-lime-600 hover:bg-lime-700 px-2 md:px-4 text-xs sm:text-sm"
-                                onClick={(e) => {
-                                e.preventDefault();
-                                handleAddToCart(product);
-                                handleAddProductToCartClick(product.id);
-                                }}
-                            >
-                                <ShoppingCart className="w-4 h-4 mr-1" />
-                                Thêm
-                            </Button>
                             </div>
                         </div>
-                        </div>
+                        ))}
                     </div>
-                    ))}
-                </div>
                 </div>
             </div>
         </div>
--- src/components/common/bannerElement/index.tsx
+++ src/components/common/bannerElement/index.tsx
@@ -3,100 +3,40 @@ import Image from "next/image";
 
 export default function BannerElement() {
     return (
-        <div className="grid grid-cols-12 gap-4 my-8 hide md:block">
-            <div className="col-span-4 h-[258] bg-orange-300 p-8 rounded-e-xl text-white group overflow-hidden relative">
-                {/* Layer background có hiệu ứng zoom */}
-                <div 
-                    className="absolute inset-0 bg-cover bg-center transition-transform duration-500 ease-out group-hover:scale-110"
-                >
-                    <Image
-                        src={'/images/organic-banner-element-1.webp'}
-                        alt="Organic Farm"
-                        layout="fill"
-                        objectFit="cover"
-                        objectPosition="center"
-                        priority
-                        quality={100}
-                    />
-                </div>
-
-                {/* Nội dung bên trong */}
-                <div className="relative z-10">
-                    <h2 className="text-3xl font-bold">
-                        <span className="text-green-600">30% </span>
-                        SALE OFF</h2>
-                    <p>Spring Fresh Deal</p>
-                    <Button 
-                        variant="outline" 
-                        className="mt-4 text-black border-white hover:bg-white hover:text-orange-300 font-medium"
-                    >
-                        Mua Ngay
-                    </Button>
-                </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 my-8">
+        {[1, 2, 3].map((item, index) => (
+            <div
+            key={index}
+            className="h-[240px] md:h-[258px] bg-orange-300 p-6 sm:p-6 md:p-8 rounded-xl text-white group overflow-hidden relative"
+            >
+            {/* Layer background có hiệu ứng zoom */}
+            <div className="absolute inset-0 bg-cover bg-center transition-transform duration-500 ease-out group-hover:scale-110">
+                <Image
+                src={`/images/organic-banner-element-${item}.webp`}
+                alt="Organic Farm"
+                fill
+                style={{ objectFit: 'cover', objectPosition: 'center' }}
+                priority
+                quality={95}
+                />
             </div>
 
-            <div className="col-span-4 h-[258] bg-orange-300 p-8 rounded-e-xl text-white group overflow-hidden relative">
-                {/* Layer background có hiệu ứng zoom */}
-                <div 
-                    className="absolute inset-0 bg-cover bg-center transition-transform duration-500 ease-out group-hover:scale-110"
+            {/* Nội dung bên trong */}
+            <div className="relative z-10">
+                <h2 className="text-xl md:text-3xl font-bold">
+                <span className="text-green-600">30% </span>
+                SALE OFF
+                </h2>
+                <p className="text-sm md:text-base">Spring Fresh Deal</p>
+                <Button
+                variant="outline"
+                className="mt-4 text-black border-white hover:bg-white hover:text-orange-300 font-medium"
                 >
-                    <Image
-                        src={'/images/organic-banner-element-2.webp'}
-                        alt="Organic Farm"
-                        layout="fill"
-                        objectFit="cover"
-                        objectPosition="center"
-                        priority
-                        quality={95}
-                    />
-                </div>
-
-                {/* Nội dung bên trong */}
-                <div className="relative z-10">
-                    <h2 className="text-3xl font-bold">
-                        <span className="text-green-600">30% </span>
-                        SALE OFF</h2>
-                    <p>Spring Fresh Deal</p>
-                    <Button 
-                        variant="outline" 
-                        className="mt-4 text-black border-white hover:bg-white hover:text-orange-300 font-medium"
-                    >
-                        Mua Ngay
-                    </Button>
-                </div>
+                Mua Ngay
+                </Button>
             </div>
-
-
-            <div className="col-span-4 h-[258] bg-orange-300 p-8 rounded-e-xl text-white group overflow-hidden relative">
-                {/* Layer background có hiệu ứng zoom */}
-                <div 
-                    className="absolute inset-0 bg-cover bg-center transition-transform duration-500 ease-out group-hover:scale-110"
-                >
-                    <Image
-                        src={'/images/organic-banner-element-3.webp'}
-                        alt="Organic Farm"
-                        layout="fill"
-                        objectFit="cover"
-                        objectPosition="center"
-                        priority
-                        quality={95}
-                    />
-                </div>
-
-                {/* Nội dung bên trong */}
-                <div className="relative z-10">
-                    <h2 className="text-3xl font-bold">
-                        <span className="text-green-600">30% </span>
-                        SALE OFF</h2>
-                    <p>Spring Fresh Deal</p>
-                    <Button 
-                        variant="outline" 
-                        className="mt-4 text-black border-white hover:bg-white hover:text-orange-300 font-medium"
-                    >
-                        Mua Ngay
-                    </Button>
-                </div>
             </div>
+        ))}
         </div>
     );
 }
